d6597939e8304b4411a090963dbe17fa187d75fe
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 04:12:52 -0700

    version1.10

---
+++ infield/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InfieldConfig(AppConfig):
+    name = 'infield'
---
+++ managedservices/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ManagedservicesConfig(AppConfig):
+    name = 'managedservices'
