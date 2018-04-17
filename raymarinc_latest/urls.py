64930232f8c2bd8107844c7df63856a086c96b8a
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 14:11:56 +0200

    version1.02

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    path('admin/', admin.site.urls),
 ]
