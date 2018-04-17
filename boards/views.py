cde6e1f1b887851fe1f94c4c2016459a81d67ba9
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 14:30:04 +0200

    version1.03

--- boards/views.py
+++ boards/views.py
@@ -5,5 +5,5 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
+    #board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
