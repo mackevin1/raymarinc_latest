4fbbb483c588c4465d9fd65c22da1888c3657b17
mackevin1 <kevin.portellas@gmail.com>
2018-04-18 00:01:50 +0200

    version1.06

--- boards/models.py
+++ boards/models.py
@@ -7,12 +7,12 @@ class Board(models.Model):
     description = models.CharField(max_length=100)
     def __str__(self):
         return self.name
-        def get_posts_count(self):
+    def get_posts_count(self):
         return Post.objects.filter(topic__board=self).count()
 
     def get_last_post(self):
         return Post.objects.filter(topic__board=self).order_by('-created_at').first()
-        
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
