6b17caa4c21edabc27b2decb52c69c3f55c68ada
mackevin1 <kevin.portellas@gmail.com>
2018-04-18 10:57:23 +0200

    version1.08

--- boards/views.py
+++ boards/views.py
@@ -72,19 +72,6 @@ def reply_topic(request, pk, topic_pk):
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
-class PostUpdateView(UpdateView):
-    model = Post
-    fields = ('message', )
-    template_name = 'edit_post.html'
-    pk_url_kwarg = 'post_pk'
-    context_object_name = 'post'
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        post.updated_by = self.request.user
-        post.updated_at = timezone.now()
-        post.save()
-        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
 @method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
