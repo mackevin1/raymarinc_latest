a61400bcc30d8da336449c5cb19ad196d00b7c49
mackevin1 <kevin.portellas@gmail.com>
2018-04-18 23:30:10 +0200

    version1.09

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from showcase.models import Project, ProjectType, Client, ProjectImage, Role
 
-admin.site.register(Project)
-admin.site.register(ProjectType)
-admin.site.register(ProjectImage)
-admin.site.register(Client)
-admin.site.register(Role)
+# Register your models here.
