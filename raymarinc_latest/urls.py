9a8c0e4146dc6cb28efd5deac483e03cbf87f48d
mackevin1 <kevin.portellas@gmail.com>
2018-04-18 10:14:44 +0200

    version1.08

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -59,5 +59,6 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     #url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     url(r'^settings/account/$', accounts_views.UserUpdateView.as_view(), name='my_account'),
-
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
+        views.PostUpdateView.as_view(), name='edit_post'),
 ]
