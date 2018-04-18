34a780fee6a6271ad917452850e809991901d2ed
mackevin1 <kevin.portellas@gmail.com>
2018-04-18 22:54:31 +0200

    version1.09

--- bns/views.py
+++ bns/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, redirect, render
-#from django.views.generic import UpdateView, ListView
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.urls import reverse
