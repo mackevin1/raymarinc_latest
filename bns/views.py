0bb41ffb20f51e40acb7d31c5fbdfa921ca2899f
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 22:18:02 +0200

    version1.5.1

--- bns/views.py
+++ bns/views.py
@@ -18,4 +18,4 @@ class ProjectListView(ListView):
 def get_queryset(self):
     self.project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
     queryset = self.project.order_by('-last_updated')
-    return render(request, 'product_list.html', {'project': project})
+    return render(request, 'project_list.html', {'project': project})
