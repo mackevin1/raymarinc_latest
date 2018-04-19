c542d5ee610a011dc035bd6cd5e5add16df66a6e
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 14:11:21 +0200

    version1.5.1

--- bns/urls.py
+++ bns/urls.py
@@ -1,11 +1,14 @@
 #from django.conf.urls.defaults import *
 from bns.models import Project
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 info_dict = {
     'queryset': Project.objects.all(),
 }
 
 urlpatterns = [
-    url(r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-    url(r'^(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+    url(r'^$', ' django.views.generic.list.ListView', info_dict),
+    url(r'^(?P<slug>[\w-]+)/$', ' django.views.generic.list.ListView', info_dict),
 ]
