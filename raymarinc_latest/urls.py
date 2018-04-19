adf415e0b2ac9d574600f893289ded3ad4379259
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 13:32:34 +0200

    version1.5.1

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -46,5 +46,5 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         views.PostUpdateView.as_view(), name='edit_post'),
     url(r'^admin/', admin.site.urls),
-
+    url(r'^bns/',views.ProjectListView.as_view(),name='project'),
 ]
