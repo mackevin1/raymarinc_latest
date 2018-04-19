7742d481a4bd23c606c4edddd8ebb23c6b3dc651
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 14:33:05 +0200

    version1.5.1

--- bns/urls.py
+++ bns/urls.py
@@ -4,11 +4,15 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from bns import views
+from accounts import views as accounts_views
+from boards import views as boards_views
+
 info_dict = {
     'queryset': Project.objects.all(),
 }
 
 urlpatterns = [
     url(r'^bns/$', views.ProductListView.as_view(), info_dict),
-    url(r'^(?P<slug>[\w-]+)/$', views.ProductListView.as_view(), info_dict),
+    url(r'^bns(?P<slug>[\w-]+)/$', views.ProductListView.as_view(), info_dict),
 ]
