bfb79511ee34e747998c1ac92dfbf3bdfd7e281f
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 13:46:02 +0200

    version1.5.1

---
+++ bns/views.py
@@ -0,0 +1,17 @@
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import UpdateView, ListView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.urls import reverse
+
+from .forms import NewTopicForm, PostForm
+from .models import Project
+
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'Project'
+    template_name = 'home.html'
