44c0babbf30299e23aa3d7d8c261b823ca514f39
mackevin1 <kevin.portellas@gmail.com>
2018-04-19 11:16:43 +0200

    version1.11

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -43,11 +43,10 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         views.PostUpdateView.as_view(), name='edit_post'),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+    url(r'^(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
 ]
-urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^(?P<slug>[\w-]+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
-)
+
 info_dict = {
     'queryset': Project.objects.all(),
 }
