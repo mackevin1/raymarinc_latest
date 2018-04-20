f3b7e867a6f1d2c96f6cfcb30bb4e56487310f58
mackevin1 <kevin.portellas@gmail.com>
2018-04-20 11:46:55 +0200

    version1.5.2

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
     #url(r'^work/(?P<slug>\d+)/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
     url(r'^work/bns/(?P<project_pk>\d+)/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
     url(r'^work/bns/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
-    url(r'^work/$', views.ProjectListView.as_view(template_name='showcase/aglans.html'), info_dict),
+    url(r'^work/$', views.ProjectListView.as_view(template_name='showcase/product_list.html'), info_dict),
     #url(r'^work/bns/(?P<product_pk>\d+)/$', views.ProjectListView.as_view(template_name='project_detail.html'), name='project'),
     #url(r'^bns/$', views.ProjectListView.as_view(template_name='product.html'), name='product'),
 ]
