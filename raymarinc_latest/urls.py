33823c99f4914a6602356d4a24a9517f4ffd9434
mackevin1 <kevin.portellas@gmail.com>
2018-04-20 11:59:40 +0200

    version1.5.2

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -50,9 +50,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'^bns/project/(?P<project_pk>\d+)/$', views.ProjectListView.as_view(template_name='project_detail.html'), info_dict),
     #url(r'^work/(?P<slug>\d+)/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
-    url(r'^work/bns/(?P<project_pk>\d+)/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
-    url(r'^work/bns/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
-    url(r'^work/$', views.ProjectListView.as_view(template_name='showcase/project_list.html'), info_dict),
+    url(r'^work/bns/(?P<project_pk>\d+)/$', views.ProjectListView.as_view(template_name='project_detail.html'), info_dict),
+    url(r'^work/bns/$', views.ProjectListView.as_view(template_name='project_detail.html'), info_dict),
+    url(r'^work/$', views.ProjectListView.as_view(template_name='project_list.html'), info_dict),
     #url(r'^work/bns/(?P<product_pk>\d+)/$', views.ProjectListView.as_view(template_name='project_detail.html'), name='project'),
     #url(r'^bns/$', views.ProjectListView.as_view(template_name='product.html'), name='product'),
 ]
