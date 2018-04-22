083bbcbe7a1040ea450da8e6b2458a404ff27b18
mackevin1 <kevin.portellas@gmail.com>
2018-04-22 21:42:19 +0200

    version1.60

--- boards/models.py
+++ boards/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
+class bns_home(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.FileField(null=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'bns_home'
+        ordering = ('-completion_date',)
+
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
