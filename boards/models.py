a1a92d6e9ad2073b7603df3745e7875d45a803f8
mackevin1 <kevin.portellas@gmail.com>
2018-04-22 21:47:48 +0200

    version1.60

--- boards/models.py
+++ boards/models.py
@@ -3,11 +3,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
+class homepage(models.Model):
+    headline1 = models.CharField(max_length=250)
+    backimage = models.FileField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    headline = models.TextField(blank=True)
+    subhead = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'homepage'
+
+
 class bns_home(models.Model):
     title = models.CharField(max_length=250)
     image = models.FileField(null=True, blank=True)
     description = models.TextField(blank=True)
-
+    headline = models.TextField(blank=True)
+    subhead = models.TextField(blank=True)
     def __str__(self):
         return self.name
     class Meta:
