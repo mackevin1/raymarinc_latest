cd07108e5cf0d5c913b7214c26047de6c35c3f5a
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 22:46:28 +0200

    version1.60

--- boards/models.py
+++ boards/models.py
@@ -94,6 +94,8 @@ class Board(models.Model):
     imagenoc2 = models.FileField(null=True, blank=True)
     imagenoc3 = models.FileField(null=True, blank=True)
     imagenoc4 = models.FileField(null=True, blank=True)
+
+
     def __str__(self):
         return self.name
     def get_posts_count(self):
@@ -144,3 +146,38 @@ class Post(models.Model):
 
 def get_message_as_markdown(self):
         return mark_safe(markdown(self.message, safe_mode='escape'))
+
+
+class Products(models.Model):
+    companyname = models.CharField(max_length=100)
+    images = models.FileField(null=True, blank=True)
+    name = models.CharField(max_length=100)
+    productid = models.CharField(max_length=100)
+    model-make = models.TextField(max_length=100)
+    category = models.CharField(max_length=100)
+    subcategory = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    value = models.CharField(max_length=100)
+    tags = models.CharField(max_length=100)
+    searchkeys = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Images(models.Model):
+    name = models.ForeignKey(Products, on_delete=models.CASCADE)
+    images = models.ForeignKey(Products, on_delete=models.CASCADE)
+    primary = models.FileField(null=True, blank=True)
+
+    def __str__(self):
+        return self.ProductName
+
+class Category(models.Model):
+    name = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Products, on_delete=models.CASCADE)
