41bf888a6a61b673bc3fd43434ba54b56987a816
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 22:53:16 +0200

    version1.60

--- boards/views.py
+++ boards/views.py
@@ -8,54 +8,8 @@ from django.utils.decorators import method_decorator
 from django.urls import reverse
 
 from .forms import NewTopicForm, PostForm
-from .models import Board, Post, Topic, Homepage, BNShome, introduction, Products, Category
+from .models import Products, Category
 from bns.models import Project
-
-class BoardListView(ListView):
-    model = Board
-    context_object_name = 'boards'
-    template_name = 'home.html'
-
-class HomepageListView(ListView):
-    model = Homepage
-    context_object_name = 'homepages'
-    template_name = 'home.html'
-
-class TopicListView(ListView):
-    model = Topic
-    context_object_name = 'topics'
-    template_name = 'topics.html'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        kwargs['board'] = self.board
-        return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
-        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-        return queryset
-
-
-class PostListView(ListView):
-    model = Post
-    context_object_name = 'posts'
-    template_name = 'topic_posts.html'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        session_key = 'viewed_topic_{}'.format(self.topic.pk)
-        if not self.request.session.get(session_key, False):
-            self.topic.views += 1
-            self.topic.save()
-            self.request.session[session_key] = True
-        kwargs['topic'] = self.topic
-        return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
-        queryset = self.topic.posts.order_by('created_at')
-        return queryset
 
 
 @login_required
@@ -125,15 +79,6 @@ class PostUpdateView(UpdateView):
         post.save()
         return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
-class ProjectListView(ListView):
-    model = Project
-    context_object_name = 'Project'
-    template_name = 'project.html'
-
-def get_queryset(self):
-    self.project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
-    queryset = self.project.order_by('-last_updated')
-    return queryset
 
 class ProductListView(ListView):
     model = Products
