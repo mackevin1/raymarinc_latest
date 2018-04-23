a02a1f684f9986c6ca7c305eacec2107afaac4d3
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 22:54:54 +0200

    version1.60

--- boards/admin.py
+++ boards/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category
+from .models import Products, Category
 
-admin.site.register(Product)
+admin.site.register(Products)
 admin.site.register(Category)
