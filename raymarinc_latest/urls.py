390de4c916664f6e256e680552efe1e025f5d83a
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 22:58:37 +0200

    version1.60

--- raymarinc_latest/urls.py
+++ raymarinc_latest/urls.py
@@ -15,7 +15,7 @@ info_dict = {
 }
 
 urlpatterns = [
-    url(r'^$', views.BoardListView.as_view(), name='home'),
+    url(r'^$', views.ProductListView.as_view(), name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
@@ -43,12 +43,12 @@ urlpatterns = [
     url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
         name='password_change_done'),
 
-    url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
-        views.PostUpdateView.as_view(), name='edit_post'),
+    #url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
+    #url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    #url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
+    #url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+    #url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
+    #    views.PostUpdateView.as_view(), name='edit_post'),
     url(r'^admin/', admin.site.urls),
     #url(r'^bns/project/(?P<project_pk>\d+)/$', views.ProjectListView.as_view(template_name='project_detail.html'), info_dict),
     #url(r'^work/(?P<slug>\d+)/$', views.ProjectListView.as_view(template_name='showcase/project_detail.html'), info_dict),
