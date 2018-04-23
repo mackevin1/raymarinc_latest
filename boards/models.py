213b8bb41e517a1ffb8fdb85e5396f7f9a75763e
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 10:56:57 +0200

    version1.60

--- boards/models.py
+++ boards/models.py
@@ -42,7 +42,7 @@ class BNShome(models.Model):
 
 class Board(models.Model):
     headline = models.CharField(max_length=250)
-    subhead = models.CharField(blank=True)
+    subhead = models.CharField(blank=True, max_length=200)
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
     def __str__(self):
