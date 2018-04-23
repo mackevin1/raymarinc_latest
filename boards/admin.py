708497729c7eec5bdc74d8376ae352fd1fe5fda6
mackevin1 <kevin.portellas@gmail.com>
2018-04-23 09:36:34 +0200

    version1.60

--- boards/admin.py
+++ boards/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Board, Topic, BNShome, Homepage
+from .models import Board, Topic, BNShome, Homepage, introduction
 
 admin.site.register(Board)
 admin.site.register(Topic)
 admin.site.register(BNShome)
 admin.site.register(Homepage)
+admin.site.register(introduction)
