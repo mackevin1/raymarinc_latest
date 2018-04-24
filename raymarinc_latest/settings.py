5184c2b6909f072b3fc4e97b4c568fbd90d8663e
mackevin1 <kevin.portellas@gmail.com>
2018-04-24 17:14:40 +0200

    version1.60

--- raymarinc_latest/settings.py
+++ raymarinc_latest/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'raymarinc_latest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3_latest'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3_latestapril'),
     }
 }
 
