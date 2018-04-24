2bc19b96fa42145d3643d84287bf46283d78debc
mackevin1 <kevin.portellas@gmail.com>
2018-04-24 09:08:34 +0200

    version1.60

--- boards/models.py
+++ boards/models.py
@@ -178,5 +178,5 @@ class Category(models.Model):
         return self.name
 
 class SubCategory(models.Model):
-    category = models.ForeignKey(Category, related_name='+', on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, through'Products', on_delete=models.CASCADE)
     subcategory = models.ForeignKey(Products, related_name='+', on_delete=models.CASCADE)
