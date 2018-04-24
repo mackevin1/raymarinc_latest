46f4c469115295643f6e1f728db8c97bf82b5e77
mackevin1 <kevin.portellas@gmail.com>
2018-04-24 23:37:50 +0200

    version1.73

--- boards/views.py
+++ boards/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.urls import reverse
 
-from .models import Board, Products, Category, ProductImages
+from .models import Board, Category, ProductImages
 from bns.models import Project
 
 class BoardListView(ListView):
@@ -32,16 +32,6 @@ class ProductImagesListView(ListView):
     template_name = 'home.html'
     paginate_by = 20
 
-class HomepageListView(ListView):
-    model = Homepage
-    context_object_name = 'homepages'
-    template_name = 'home.html'
-
-class TopicListView(ListView):
-    model = Topic
-    context_object_name = 'topics'
-    template_name = 'topics.html'
-    paginate_by = 20
 
     def get_context_data(self, **kwargs):
         kwargs['board'] = self.board
@@ -59,26 +49,6 @@ class TopicListView(ListView):
     def get_queryset(self):
         self.board = get_object_or_404(Products, pk=self.kwargs.get('pk'))
         queryset = self.products.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-        return queryset
-
-class PostListView(ListView):
-    model = Post
-    context_object_name = 'posts'
-    template_name = 'topic_posts.html'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        session_key = 'viewed_topic_{}'.format(self.topic.pk)
-        if not self.request.session.get(session_key, False):
-            self.topic.views += 1
-            self.topic.save()
-            self.request.session[session_key] = True
-        kwargs['topic'] = self.topic
-        return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
-        queryset = self.topic.posts.order_by('created_at')
         return queryset
 
 
@@ -130,24 +100,6 @@ def reply_topic(request, pk, topic_pk):
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
 
-@method_decorator(login_required, name='dispatch')
-class PostUpdateView(UpdateView):
-    model = Post
-    fields = ('message', )
-    template_name = 'edit_post.html'
-    pk_url_kwarg = 'post_pk'
-    context_object_name = 'post'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(created_by=self.request.user)
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        post.updated_by = self.request.user
-        post.updated_at = timezone.now()
-        post.save()
-        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
 class ProjectListView(ListView):
     model = Project
